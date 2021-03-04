b96e95b5d5af7b16dbcaae93355e32f6149da72f
Mohammad Altamimi <m98altamimi@gmail.com>
2021-03-04 14:45:50 +0200

    dynamics

---
+++ dynamic.js
@@ -0,0 +1,28 @@
+alert("Hello and Welcome to my website.In this we page you will be able to view some of the most beautiful quotes");
+
+// prompt("Please enter your name");
+// prompt("Please state your age");
+
+if (confirm("You are about to join the most awesome quotes website.Are you ready?"))
+{
+    alert("Here we go!")
+}
+else
+{
+    var Reason =prompt("why?")
+}
+
+var Name= prompt("Please enter your name");
+var Age= prompt("Please state your age")
+
+console.log(Name);
+console.log(Age);
+
+if (Age!=18 && Age<18)
+{
+    alert ("leave!")
+}
+else
+{
+alert("Welcome!")
+}
